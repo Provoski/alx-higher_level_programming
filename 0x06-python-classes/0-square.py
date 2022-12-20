c5deff3a8d52d79c7f8cf19df5da1c54165c0f4d
Provoski <provo.iyen27@gmail.com>
2022-12-20 06:45:10 -0800

    task 0 correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" the Square class is an empty class.
-it does nothing """
 class Square:
+    """the Square class is an empty class.
+it does nothing"""
     pass
 square = Square()
