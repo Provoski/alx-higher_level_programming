c6a6340433bfecb9366675ca47adaa3bf9089445
Provoski <provo.iyen27@gmail.com>
2022-12-20 07:25:38 -0800

    task 1 added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""it defines a Square class base on 0-square.py"""
+class Square:
+    """the Square class define a square with a private intance attribute called size"""
+    def __init__(self, size):
+        self.__size = size #private attribute
+
