66ec745b7f0e031b95ae762bad97c99413bf9e0d
Provoski <provo.iyen27@gmail.com>
2022-12-26 11:48:36 -0800

    task 2 added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,19 +9,18 @@ def safe_print_list(my_list=[], x=0):
             for item in my_list[0:x]:
                 print(item, end="")
                 count += 1
-                if count == x:
-                    print()
+            print()
             return count
         elif lenght == 0:
             for items in my_list:
                 print(items)
+            print()
             return count
         else:
             for items in my_list:
                 print(items, end="")
                 count += 1
-                if count == lenght:
-                    print()
+            print()
             return count
     except IndexError:
         print("an  Error occurred")
