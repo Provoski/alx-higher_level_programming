97853c67034722819dab0a042500caa38cb119d9
Provoski <provo.iyen27@gmail.com>
2022-12-26 08:16:04 -0800

    task 0 correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,18 +2,21 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     lenght = 0
-    for i in my_list:
-        lenght += 1
-    if (x-1) < lenght:
-        for item in my_list[0:x]:
-            print(item, end="")
-            count += 1
-            if count == x:
-                print()
-    else:
-        for items in my_list:
-            print(items, end="")
-            count += 1
-            if count == lenght:
-                print()
+    try:
+        for i in my_list:
+            lenght += 1
+        if (x-1) < lenght:
+            for item in my_list[0:x]:
+                print(item, end="")
+                count += 1
+                if count == x:
+                    print()
+        else:
+            for items in my_list:
+                print(items, end="")
+                count += 1
+                if count == lenght:
+                    print()
+    except:
+        pass
     return count
