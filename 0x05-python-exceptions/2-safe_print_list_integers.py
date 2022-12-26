ee451b39f3807cc725f8686a662b36ff026a2ecf
Provoski <provo.iyen27@gmail.com>
2022-12-26 11:52:08 -0800

    task 2 correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,12 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     list_lenght = 0
     item = 0
-    for i in my_list:
-        list_lenght += 1
-    for item in range(x):
-        if isinstance(my_list[item], int):
-            print(my_list[item], end="")
-            count += 1
-    print()
+    try:
+        for item in range(x):
+            if isinstance(my_list[item], int):
+                print(my_list[item], end="")
+                count += 1
+        print()
+    except TypeError as e:
+        print("type not supported")
     return count
