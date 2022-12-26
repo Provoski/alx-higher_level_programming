0b224b78aeb9f62e04c7fb508ecc1d120425d70d
Provoski <provo.iyen27@gmail.com>
2022-12-26 08:38:34 -0800

    task 0 correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,7 +12,8 @@ def safe_print_list(my_list=[], x=0):
                 if count == x:
                     print()
         elif lenght == 0:
-            print(0)
+            for items in my_list:
+                print(items)
         else:
             for items in my_list:
                 print(items, end="")
