1292edd5cedb5f4ea80ef277d26143e9bc646c82
Provoski <provo.iyen27@gmail.com>
2022-12-26 13:22:31 -0800

    task 3 added

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a/b
+        return (result)
+    except:
+        return (None)
+    finally:
+        if result > 0:
+            print("Inside result: {:.1f}".format(result))
+        else:
+            print("Inside result: None")
