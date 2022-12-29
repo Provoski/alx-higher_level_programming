931cf8a3960a84ee9056b0361ea3544974032146
Provoski <provo.iyen27@gmail.com>
2022-12-29 05:57:27 -0800

    5-no_c.py correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	new_string = my_string.translate({ord(char): None for char in "cC"})
-	return new_string
+    new_string = my_string.translate({ord(char): None for char in "cC"})
+    return new_string
