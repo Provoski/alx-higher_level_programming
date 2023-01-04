7ad7f3284d0b84a45bc048927b38f6bee4d4d360
Provoski <provo.iyen27@gmail.com>
2023-01-04 02:20:47 -0800

    advance task 101-locked_class.py correction

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+# module 101-locked_class has one class
 class LockedClass:
+    """has no class or object.
+    It prevent the user from dynamically
+    creating new instance attribute except first_name attribute"""
+
     __slots___ = ['first_name']
