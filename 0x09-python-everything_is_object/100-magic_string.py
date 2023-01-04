67c6179ea9dde0c4e9ea9af51acd91c01c10d506
Provoski <provo.iyen27@gmail.com>
2023-01-04 02:12:53 -0800

    advance task 29 and 30 added

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    string = "BestSchool"
-    return string
+def magic_string(my_string=[]):
+    my_string.append("BestSchool")
+    return (', ' .join(my_string))
