dbcf850103c0dfebaf9819098d00cd7fb24af5d9
Provoski <provo.iyen27@gmail.com>
2023-01-04 05:10:13 -0800

    task 2 correction on more classes task

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,38 +11,51 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         if isinstance(width, int) is not True:
             raise TypeError("width must be an integer")
-        if width < 0:
+        elif width < 0:
             raise ValueError("width must be >= 0")
-        if isinstance(height, int) is not True:
+        elif isinstance(height, int) is not True:
             raise ValueError("height must be an integer")
-        if height < 0:
+        elif height < 0:
             raise ValueError("height must be >= 0")
-        self.__width = width
-        self.__height = height
+        else:
+            self.__width = width
+            self.__height = height
 
     @property
     def width(self):
-        return self.__width
+        if isinstance(self.__width, int) is not True:
+            raise TypeError("width must be an integer")
+        elif self.__width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            return self.__width
 
     @width.setter
     def width(self, value):
         if isinstance(value, int) is not True:
-            raise ValueError("width must be an integer")
-        if value < 0:
+            raise TypeError("width must be an integer")
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        return self.__height
+        if isinstance(self.__height, int) is not True:
+            raise TypeError("height must be an integer")
+        elif self.__height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            return self.__height
 
     @height.setter
     def height(self, value):
         if isinstance(value, int) is not True:
             raise ValueError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
         return self.__width+self.__height
