d1174cb2e3e9dc007f231cc81574bb02f6affbd9
Provoski <provo.iyen27@gmail.com>
2023-01-04 04:29:03 -0800

    task 2 added on more classes task

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""2-rectangle module, it contains one class"""
+
+
+class Rectangle:
+    """
+    it defines a rectangle with a private insatance
+    width,height and method area and perimeter.
+    """
+
+    def __init__(self, width=0, height=0):
+        if isinstance(width, int) is not True:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if isinstance(height, int) is not True:
+            raise ValueError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is not True:
+            raise ValueError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is not True:
+            raise ValueError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width+self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2*self.__width)+(2*self.__height)
