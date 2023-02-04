536548d04da27cfc70f8eebe34583967c9baf7b1
Provoski <provo.iyen27@gmail.com>
2023-02-04 15:36:40 -0800

    task 3 in inpit validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -57,6 +57,8 @@ class Rectangle(Base):
 
             if not isinstance(value, int):
                 raise TypeError("width must be an integer")
+            if value < 1:
+                raise ValueError("width must be > 0") 
             self.__width = value
 
         @property
