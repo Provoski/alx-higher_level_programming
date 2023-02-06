3d3b25bc2e1811eacdc916de431dd72d18fb1745
Provoski <provo.iyen27@gmail.com>
2023-02-05 17:43:17 -0800

    square module

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""square.py module"""
-
+""" Square module """
 from models.rectangle import Rectangle
-"""
-Square class inherinting from Reactagle class"""
 
 
 class Square(Rectangle):
+    """
+    Square class inherinting from Rectangle class
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
-        super(Square, self).__init__(id, height)
+        super(Square, self).__init__(size, size, x, y, id)
+        self._size = size
+
+    @property
+    def size(self):
+        """ size property gette"""
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        """ size property setter """
+        self._size = size
 
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.__x, self.__y, self.size)
+        """ modifying defaul __str__ method """
+        txt = "[Square] ({}) {}/{} - {}"
+        return txt.format(self.id, self.x, self.y, self.width)
