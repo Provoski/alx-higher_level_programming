a969e70fe06b3e53e77edd613b6d01999aa56b9c
Provoski <provo.iyen27@gmail.com>
2023-03-13 06:56:26 -0700

    task 4 on import module tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__  == "__main__":
-	args_no = len(argv)
-	add = 0
-	add = int(add)
-	for i in range(1, args_no):
-		add = add + int(argv[i])
-	print("{0}".format(add))
 
-	
+
+if __name__ == "__main__":
+    args_no = len(argv)
+    add = 0
+    add = int(add)
+    for i in range(1, args_no):
+        add = add + int(argv[i])
+        print("{0}".format(add))
