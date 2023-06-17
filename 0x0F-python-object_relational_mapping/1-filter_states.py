fa6963f40afcb3bd20fa5790188b1dc1e1142bb3
Provoski <provo.iyen27@gmail.com>
2023-06-17 12:53:32 +0000

    update 1-filter_states.py and 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,17 +2,18 @@
 """filter_state module."""
 import sys
 import MySQLdb
-"""
-this script list all states with the name starting
-with N from hbtn_0e_o_usa database.
-"""
 
 
 if __name__ == '__main__':
+    """
+    this script list all states with the name starting
+    with N from hbtn_0e_o_usa database.
+    """
     arguments = sys.argv
     username = arguments[1]
     password = arguments[2]
     database = arguments[3]
+
     conn = MySQLdb.connect(
             host="localhost",
             port=3306, user=username,
@@ -20,9 +21,11 @@ if __name__ == '__main__':
             db=database,
             charset="utf8")
     cur = conn.cursor()
+
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query = cur.fetchall()
     for row in query:
         print(row)
+
     cur.close()
     conn.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""2-my_filter_state module."""
 import sys
 import MySQLdb
-"""
-2-my_filter_state module.
-this script list all states matching the state name
-argument from hbtn_0e_o_usa database.
-"""
 
 
 if __name__ == '__main__':
+    """
+    this script list all states matching the state name
+    argument from hbtn_0e_o_usa database.
+    """
+
     arguments = sys.argv
     username = arguments[1]
     password = arguments[2]
@@ -21,10 +22,12 @@ if __name__ == '__main__':
             db=database,
             charset="utf8")
     cur = conn.cursor()
+
     sql = "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC"
     cur.execute(sql, (state,))
     query = cur.fetchall()
     for row in query:
         print(row)
+
     cur.close()
     conn.close()
